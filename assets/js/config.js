b55c369e486afbd586e0b8de69409e8b511bf03e
khali <106910594+phmkhali@users.noreply.github.com>
2026-07-16 09:44:15 +0200

    clean code

---
+++ assets/js/config.js
@@ -0,0 +1,13 @@
+const firebaseConfig = {
+  databaseURL: "https://hi-baby-e6e24-default-rtdb.firebaseio.com",
+};
+
+const COOLDOWN_MS = 60 * 1000;
+const TIME_ZONES = {
+  khali: "Europe/Berlin",
+  lewis: "America/New_York",
+};
+const GAME_TZ = "America/Toronto";
+const FRANCE_DATE = new Date(2026, 7, 12);
+const CALL_BLOCK_MS = 45 * 60 * 1000;
+const CALL_BLOCK_PTS = 45;
